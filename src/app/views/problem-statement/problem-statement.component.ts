7f8ec467319d747272c4a866e03de2536bdb26ca
visup <elisa.tramontidisopra@gmail.com>
2018-04-12 17:06:12 +0200

    Statements: structure

--- src/app/views/problem-statement/problem-statement.component.ts
+++ src/app/views/problem-statement/problem-statement.component.ts
@@ -8,6 +8,28 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ProblemStatementComponent {
 
+  select = [
+    {label: 'select1', link:''},
+    {label: 'select2', link:''},
+    {label: 'select3', link:''}
+  ]
+  selectMachine = [
+    {label: 'machine1', link:''},
+    {label: 'machine2', link:''},
+    {label: 'machine3', link:''}
+  ]
+  problem = {
+    title: 'Untitled problem',
+    owner: '',
+    statement: '',
+    where: 'test where',
+    when: 'test when',
+    state: 'test future state',
+    measure: 'test measure',
+    machine: '',
+    tags: 'Enter a new tag'
+  }
+
   constructor(
     private auth: AuthService,
     private loadingService: LoadingService,
