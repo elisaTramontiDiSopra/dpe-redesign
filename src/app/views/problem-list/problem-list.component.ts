7d3d0d9bd89703f07ede1543e855dc1d8833b6f1
visup <elisa.tramontidisopra@gmail.com>
2018-04-12 14:18:17 +0200

    Problem List: structure

--- src/app/views/problem-list/problem-list.component.ts
+++ src/app/views/problem-list/problem-list.component.ts
@@ -10,6 +10,21 @@ export class ProblemListComponent {
   user: any = {}
   isLoading: boolean;
 
+  select = [
+    {label: 'select1', link:''},
+    {label: 'select2', link:''},
+    {label: 'select3', link:''}
+  ]
+  problems = [
+    {title: 'Untitled problem', description: 'Decscription 1', responsable: 'Jane Doe', creationDate: '12-12-2012', progress: 0.5},
+    {title: 'Untitled problem', description: 'Decscription 1', responsable: 'Jane Doe', creationDate: '12-12-2012', progress: 1},
+    {title: 'Untitled problem', description: 'Decscription 1', responsable: 'Jane Doe', creationDate: '12-12-2012', progress: 0.3}
+  ]
+  actions = {
+    done: 4,
+    total: 6
+  }
+
   constructor(
     private auth: AuthService,
     private loadingService: LoadingService,
@@ -17,5 +32,8 @@ export class ProblemListComponent {
   ) {
   }
 
+  deleteProblem() {
+    
+  }
   
 }
