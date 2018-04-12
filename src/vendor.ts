fd2d5cbe7aab9a8759d032d85c06b0dd0a5fa663
visup <elisa.tramontidisopra@gmail.com>
2018-04-12 10:47:31 +0200

    First: commit

---
+++ src/vendor.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+// import 'jquery';
+// import 'mdi/css/materialdesignicons.css';
+import * as WebFont from 'webfontloader';
+// import 'bootstrap/js/dist/collapse';
+WebFont.load({
+  google: {
+    families: [
+      'Source Sans Pro:300,400,600,700'
+    ]
+  }
+});
